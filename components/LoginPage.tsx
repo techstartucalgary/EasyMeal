dded15d2bb7c4edff4527151110ee9979ded529c
NolanChan1 <nolanchan1@gmail.com>
2022-11-04 18:13:04 -0600

    Added inputs to LoginPage

--- components/LoginPage.tsx
+++ components/LoginPage.tsx
@@ -4,6 +4,8 @@ import {
   Text,
   View,
   SafeAreaView,
+  TextInput,
+  Button,
   StatusBar,
   Platform,
 } from "react-native";
@@ -14,6 +16,8 @@ import AntDesign from "@expo/vector-icons/AntDesign";
 function LoginPage() {
   const [fontsLoaded] = useFonts({
     "Inter-SemiBold": require("../assets/fonts/Inter-SemiBold.ttf"),
+    "Inter-Medium": require("../assets/fonts/Inter-Medium.ttf"),
+    "Inter-Regular": require("../assets/fonts/Inter-Regular.ttf"),
   });
 
   if (!fontsLoaded) {
@@ -31,6 +35,20 @@ function LoginPage() {
         />
         <Text style={styles.loginText}>Login</Text>
       </View>
+      <TextInput
+        style={[styles.loginInput, styles.emailInput]}
+        placeholder="Email"
+      />
+      <TextInput
+        style={[styles.loginInput, styles.passwordInput]}
+        placeholder="Password"
+      />
+      <Text style={styles.forgotPassword}>Forgot your password?</Text>
+      <Text style={styles.loginButton}>Login</Text>
+      <View style={styles.signupTextContainer}>
+        <Text style={styles.signupText}>Don't have an account yet?</Text>
+        <Text style={[styles.signupText, styles.signupButton]}> Sign Up</Text>
+      </View>
     </SafeAreaView>
   );
 }
@@ -42,9 +60,32 @@ const styles = StyleSheet.create({
     position: "absolute",
     left: 20,
   },
+  emailInput: {
+    marginTop: 56,
+  },
+  forgotPassword: {
+    color: "#6536F9",
+    fontFamily: "Inter-Regular",
+    fontSize: 13,
+    marginLeft: 20,
+    marginTop: 12,
+  },
   loginContainer: {
     flex: 1,
-    backgroundColor: "#333",
+    backgroundColor: "#fff",
+  },
+  loginButton: {
+    fontFamily: "Inter-SemiBold",
+    fontSize: 18,
+    textAlign: "center",
+    textAlignVertical: "center",
+    color: "#fcfcfc",
+    backgroundColor: "#6536F9",
+    borderRadius: 16,
+    height: 56,
+    marginLeft: 16,
+    marginRight: 16,
+    marginTop: 36,
   },
   loginHeader: {
     flexDirection: "row",
@@ -55,9 +96,40 @@ const styles = StyleSheet.create({
     paddingTop: 16,
     paddingBottom: 16,
   },
+  loginInput: {
+    backgroundColor: "#fff",
+    borderColor: "#f1f1f1",
+    borderWidth: 2,
+    borderRadius: 16,
+    height: 56,
+
+    marginLeft: 16,
+    marginRight: 16,
+    paddingLeft: 16,
+    fontFamily: "Inter-Regular",
+    fontSize: 16,
+    textAlignVertical: "center",
+  },
   loginText: {
     fontFamily: "Inter-SemiBold",
     fontSize: 18,
     color: "#212325",
+  },
+  passwordInput: {
+    marginTop: 24,
+  },
+  signupButton: {
+    color: "#6536F9",
+  },
+  signupText: {
+    fontFamily: "Inter-Medium",
+    fontSize: 16,
+    color: "#91919F",
+  },
+  signupTextContainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "center",
+    marginTop: 36,
   },
 });
