7d444acb70b81f67235d84a0353f6b405c891b83
Nemanja Grujic <luxisoop@gmail.com>
2022-12-01 17:50:34 -0700

    initial home page work

--- pages.ts
+++ pages.ts
@@ -1,7 +1,12 @@
+import HomePage from 'components/HomePage/HomePage';
 import LoginPage from './components/Authentication/LoginPage';
 import SignUpPage from './components/Authentication/SignUpPage';
 
 export const publicPages = [
+  {
+    page: HomePage,
+    name: 'Home',
+  },
   {
     page: LoginPage,
     name: 'Login',
