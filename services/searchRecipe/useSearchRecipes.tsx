bf6499c8476db795295d5e2a826cfd0168d5dec8
Mauricio Murillo Gonzales <mauriciomurillogonzales@Mauricios-MacBook-Pro.local>
2023-01-26 18:33:16 -0700

    Fixed the recipe search hook to account for query and number of results

--- services/searchRecipe/useSearchRecipes.tsx
+++ services/searchRecipe/useSearchRecipes.tsx
@@ -8,6 +8,8 @@ export const useRecipes = ({
   type,
   maxReadyTime,
   enabled,
+  query,
+  number,
 }: RecipesType) => {
   const [recipeList, setRecipeList] = useState<RecipeListType | undefined>(
     undefined,
@@ -18,7 +20,7 @@ export const useRecipes = ({
     if (enabled === true || enabled === undefined) {
       setIsLoading(true);
       fetch(
-        `https://api.spoonacular.com/recipes/complexSearch?apiKey=${SPOON_API_KEY}&cuisine=${cuisine}&diet=${diet}&type=${type}&maxReadyTime=${maxReadyTime}`,
+        `https://api.spoonacular.com/recipes/complexSearch?apiKey=${SPOON_API_KEY}&query=${query}&cuisine=${cuisine}&diet=${diet}&type=${type}&maxReadyTime=${maxReadyTime}&number=${number}`,
       )
         .then((response) => response.json())
         .then((data) => {
@@ -26,7 +28,7 @@ export const useRecipes = ({
           setRecipeList(data);
         });
     }
-  }, [cuisine, diet, maxReadyTime, type, enabled]);
+  }, [cuisine, diet, maxReadyTime, type, enabled, query, number]);
   // funcion, array de depndencias
   return { recipeList, isLoading };
 };
