3b351a3714bfde82801af9fc8438c7cae10fb151
NolanChan1 <nolanchan1@gmail.com>
2023-02-04 23:24:26 -0700

    Add different pantry types

--- components/Pantry/PantryPage.tsx
+++ components/Pantry/PantryPage.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   StyleSheet,
   Text,
@@ -6,11 +6,18 @@ import {
   SafeAreaView,
   StatusBar,
   Platform,
+  Pressable,
 } from 'react-native';
 
+import { AntDesign } from '@expo/vector-icons';
 import { useFonts } from 'expo-font';
 
+import { pantryTypes } from './pantry-types';
+
 const PantryPage = () => {
+  const [selectedPantryType, setSelectedPantryType] =
+    useState<typeof pantryTypes[number]['id']>(0);
+
   const [fontsLoaded] = useFonts({
     'Inter-Bold': require('../../assets/fonts/Inter-Bold.ttf'),
     'Inter-SemiBold': require('../../assets/fonts/Inter-SemiBold.ttf'),
@@ -25,8 +32,37 @@ const PantryPage = () => {
   return (
     <SafeAreaView style={styles.pantryPageContainer}>
       <View style={styles.pantryPageHeader}>
-        <Text>TEST</Text>
+        <Text style={styles.pantryHeaderText}>Pantry</Text>
+        <View style={styles.pantryPageRightHeader}>
+          <AntDesign name="search1" size={24} color="#3E5481" />
+          <Pressable style={styles.addItemButton}>
+            <AntDesign
+              name="pluscircle"
+              size={20}
+              color="#000000"
+              style={styles.addItemIcon}
+            />
+            <Text style={styles.addItemText}>Add item</Text>
+          </Pressable>
+        </View>
       </View>
+      <View style={styles.pantryTypeHeader}>
+        {pantryTypes.map((pantryType) => (
+          <Pressable
+            onPress={() => setSelectedPantryType(pantryType.id)}
+            style={styles.pantryTypeVContainer}
+          >
+            <View style={styles.pantryTypeHContainer}>
+              <Text style={styles.pantryTypeText}>{pantryType.title}</Text>
+              <Text style={styles.pantryTypeCount}>{pantryType.count}</Text>
+            </View>
+            {selectedPantryType === pantryType.id && (
+              <View style={styles.pantryTypeBar}></View>
+            )}
+          </Pressable>
+        ))}
+      </View>
+      <View style={styles.pantryResultsContainer}></View>
     </SafeAreaView>
   );
 };
@@ -34,17 +70,103 @@ const PantryPage = () => {
 export default PantryPage;
 
 const styles = StyleSheet.create({
+  addItemButton: {
+    marginLeft: 24,
+    height: 40,
+    borderRadius: 32,
+
+    flexDirection: 'row',
+    alignItems: 'center',
+
+    backgroundColor: '#EDEDED',
+  },
+  addItemIcon: {
+    marginLeft: 8,
+  },
+  addItemText: {
+    marginLeft: 8,
+    marginRight: 8,
+
+    fontFamily: 'Inter-SemiBold',
+    fontSize: 12,
+    color: '#000000',
+  },
+  pantryHeaderText: {
+    position: 'relative',
+    fontFamily: 'Inter-SemiBold',
+    fontSize: 28,
+    color: '#222222',
+  },
   pantryPageContainer: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#F8F8F8',
   },
   pantryPageHeader: {
     flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#fff',
+    alignItems: 'flex-end',
     marginTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
-    paddingTop: 16,
-    paddingBottom: 16,
+
+    paddingTop: 12,
+    marginLeft: 24,
+    marginRight: 24,
+  },
+  pantryPageRightHeader: {
+    position: 'absolute',
+    right: 0,
+
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  pantryResultsContainer: {
+    position: 'relative',
+    left: 0,
+    top: -2,
+
+    flex: 1,
+    flexDirection: 'column',
+
+    backgroundColor: '#FFFFFF',
+  },
+  pantryTypeBar: {
+    height: 4,
+    borderRadius: 2,
+    backgroundColor: '#6536F9',
+  },
+  pantryTypeCount: {
+    fontFamily: 'Inter-SemiBold',
+    fontSize: 9,
+    color: '#6536F9',
+
+    paddingVertical: 2,
+    paddingHorizontal: 6,
+    borderRadius: 10,
+    backgroundColor: '#EEE5FF',
+
+    marginRight: 4,
+  },
+  pantryTypeHContainer: {
+    flexDirection: 'row',
+
+    marginBottom: 8,
+  },
+  pantryTypeHeader: {
+    flexDirection: 'row',
+    alignItems: 'flex-start',
+    justifyContent: 'space-between',
+
+    marginTop: 20,
+    marginLeft: 24,
+    marginRight: 24,
+  },
+  pantryTypeText: {
+    fontFamily: 'Inter-SemiBold',
+    fontSize: 12,
+    color: '#6536F9',
+
+    marginRight: 4,
+    marginLeft: 4,
+  },
+  pantryTypeVContainer: {
+    flexDirection: 'column',
   },
 });
